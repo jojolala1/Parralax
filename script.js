0ae5e64d2c859f454dba36ac094679a5b69a089e
jojolala1 <131523659+jojolala1@users.noreply.github.com>
2024-09-07 15:36:07 +0200

    good parralax

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const parralax_el = document.querySelectorAll('.parralax');
 
-let xValue = 0, yValue = 0
+let xValue = 0, 
+    yValue = 0
+
+    let rotateDegree =0;
 
 window.addEventListener("mousemove", (e)=>{
     xValue = e.clientX - window.innerWidth/2
     yValue = e.clientY - window.innerHeight/2
 
+    rotateDegree =( xValue / ( window.innerWidth/2)) * 20;
+
     parralax_el.forEach((el) => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
+        let speedz = el.dataset.speedz
+        let isInLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth/2 ? 1 : -1;
 
-        el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue *speedy}px))`;
+        let zValue = (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+
+        el.style.transform = `translateX(calc(-50% + ${
+            -xValue * speedx
+        }px)) translateY(calc(-50% + ${
+            yValue *speedy
+        }px)) perspective(2300px) translateZ(${zValue* speedz}px)`;
     })
 })
